a3ea62b6633ad6639a0f0b99fb612a2f309654a7
Víctor Rolando Fernández Sánchez <vrfernandezsanchez@educa.madrid.org>
2026-06-23 08:50:58 +0200

    Agregar archivos de proyecto.

---
+++ tareas-api/Controllers/TareasController.cs
@@ -0,0 +1,21 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using tareas_api.Models;
+
+namespace tareas_api.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class TareasController : ControllerBase
+    {
+        [HttpGet]
+        public List<Tarea>ObtenerTodas()
+        {
+            return new List<Tarea>
+            {
+                new Tarea { Id = 1, Titulo = "Aprender APIs", Completada = false },
+                new Tarea { Id = 2, Titulo = "Probar Swagger", Completada = true }
+            };
+        }
+    }
+}
---
+++ tareas-api/Models/Tarea.cs
@@ -0,0 +1,11 @@
+﻿namespace tareas_api.Models
+{
+    public class Tarea
+    {
+        public int Id { get; set; }
+        public string Titulo { get; set; }
+        public string Descripcion { get; set; }
+        public bool Completada { get; set; }
+        public DateTime FechaCreacion { get; set; }
+    }
+}
