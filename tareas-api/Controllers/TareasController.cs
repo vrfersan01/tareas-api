e9c78536e71bba62edaac4664cfdf05f36974fe2
Víctor Rolando Fernández Sánchez <vrfernandezsanchez@educa.madrid.org>
2026-06-24 09:03:54 +0200

    Completa el CRUD de la API: POST, PUT y DELETE

--- tareas-api/Controllers/TareasController.cs
+++ tareas-api/Controllers/TareasController.cs
@@ -1,5 +1,6 @@
 ﻿using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
+using System.Data.SqlTypes;
 using tareas_api.Models;
 
 namespace tareas_api.Controllers
@@ -25,6 +26,52 @@ namespace tareas_api.Controllers
 
             else
                 resultado = Ok(t);
+
+            return resultado;
+        }
+
+        [HttpPost]
+        public ActionResult<Tarea> CrearTarea([FromBody] Tarea nueva)
+        {
+            nueva.Id = _siguienteId;
+            _siguienteId++;
+            nueva.FechaCreacion = DateTime.Now;
+            _tareas.Add(nueva);
+
+            return CreatedAtAction(nameof(ObtenerPorId), new { id = nueva.Id}, nueva); 
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarTarea(int id, [FromBody] Tarea datos)
+        {
+            IActionResult resultado;
+            Tarea tarea = _tareas.FirstOrDefault(t => t.Id == id);
+
+            if( tarea == null )
+                resultado = NotFound();
+            else
+            {
+                tarea.Titulo = datos.Titulo;
+                tarea.Descripcion = datos.Descripcion;
+                tarea.Completada = datos.Completada;
+                resultado = NoContent();
+            }
+
+            return resultado;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult BorrarTarea(int id)
+        {
+            IActionResult resultado;
+            Tarea tarea = _tareas.FirstOrDefault(t => t.Id == id);
+            if(tarea == null )
+                resultado = NotFound();
+            else
+            {
+                _tareas.Remove(tarea);
+                resultado = NoContent();
+            }
 
             return resultado;
         }
