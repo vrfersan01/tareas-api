b9b2657b4ba8e8f1c4e34f070d2f2b478338a915
Víctor Rolando Fernández Sánchez <vrfernandezsanchez@educa.madrid.org>
2026-06-24 08:32:53 +0200

    Anade GET de todas y GET por id con almacen en memoria

--- tareas-api/Controllers/TareasController.cs
+++ tareas-api/Controllers/TareasController.cs
@@ -9,13 +9,33 @@ namespace tareas_api.Controllers
     public class TareasController : ControllerBase
     {
         [HttpGet]
-        public List<Tarea>ObtenerTodas()
+        public ActionResult<List<Tarea>> ObtenerTodas()
         {
-            return new List<Tarea>
-            {
-                new Tarea { Id = 1, Titulo = "Aprender APIs", Completada = false },
-                new Tarea { Id = 2, Titulo = "Probar Swagger", Completada = true }
-            };
+            return Ok(_tareas);
         }
+
+        [HttpGet("{id}")]
+        public ActionResult<Tarea> ObtenerPorId(int id)
+        {
+            ActionResult<Tarea> resultado;
+            Tarea t = _tareas.FirstOrDefault(t => t.Id == id);
+
+            if(t == null)
+                resultado = NotFound();
+
+            else
+                resultado = Ok(t);
+
+            return resultado;
+        }
+
+
+
+        private static List<Tarea> _tareas = new List<Tarea>
+        {
+            new Tarea { Id = 1, Titulo = "Aprender APIs", Completada = false },
+            new Tarea { Id = 2, Titulo = "Probar Swagger", Completada = true }
+        };
+        private static int _siguienteId = 3;
     }
 }
