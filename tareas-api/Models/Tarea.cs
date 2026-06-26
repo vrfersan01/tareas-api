5dbadd61ff2a0a05e88a1a06a48f2f49a58a8013
Víctor Rolando Fernández Sánchez <vrfernandezsanchez@educa.madrid.org>
2026-06-26 08:06:25 +0200

    Anade validacion del modelo y codigos de estado correctos

--- tareas-api/Models/Tarea.cs
+++ tareas-api/Models/Tarea.cs
@@ -1,8 +1,13 @@
-﻿namespace tareas_api.Models
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace tareas_api.Models
 {
     public class Tarea
     {
         public int Id { get; set; }
+
+        [Required(ErrorMessage = "El titulo es obligatorio")]
+        [StringLength(150)]
         public string Titulo { get; set; }
         public string Descripcion { get; set; }
         public bool Completada { get; set; }
