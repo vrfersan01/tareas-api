79a0bee977b1753ead4398fe4d6a8694f21e816f
Víctor Rolando Fernández Sánchez <vrfernandezsanchez@educa.madrid.org>
2026-06-30 08:54:02 +0200

    CRUD completo contra MySQL con EF Core (POST, PUT, DELETE)

--- tareas-api/Controllers/TareasController.cs
+++ tareas-api/Controllers/TareasController.cs
@@ -25,7 +25,7 @@ namespace tareas_api.Controllers
         }
 
         [HttpGet("{id}")]
-        public ActionResult<Tarea> BuscarPorId(int id)
+        public ActionResult<Tarea> ObtenerPorId(int id)
         {
             Tarea tarea = _context.Tareas.Find(id);
             ActionResult<Tarea> resultado;
@@ -37,6 +37,53 @@ namespace tareas_api.Controllers
                 resultado = Ok(tarea);
 
             return resultado;
+        }
+
+        [HttpPost]
+        public ActionResult<Tarea> Crear([FromBody] Tarea tarea)
+        {
+            tarea.FechaCreacion = DateTime.Now;
+            _context.Tareas.Add(tarea);
+            _context.SaveChanges();
+
+            return CreatedAtAction(nameof(ObtenerPorId),
+                                    new { id = tarea.Id }, tarea);
+        }
+
+        [HttpPut("{id}")]
+        public IActionResult ActualizarTarea(int id, [FromBody]Tarea datos)
+        {
+            IActionResult result;
+
+            Tarea tarea = _context.Tareas.Find(id);
+            if(tarea == null)
+                result = NotFound();
+            else
+            {
+                tarea.Titulo = datos.Titulo;
+                tarea.Descripcion = datos.Descripcion;
+                tarea.Completada = datos.Completada;
+                _context.SaveChanges();
+                result = NoContent();
+            }
+            return result;
+        }
+
+        [HttpDelete("{id}")]
+        public IActionResult BorrarTarea(int id)
+        {
+            IActionResult result;
+            Tarea tarea = _context.Tareas.Find(id);
+
+            if(tarea == null)
+                result= NotFound();
+            else
+            {
+                _context.Tareas.Remove(tarea);
+                _context.SaveChanges();
+                result= NoContent();
+            }
+            return result;
         }
     }
 }
